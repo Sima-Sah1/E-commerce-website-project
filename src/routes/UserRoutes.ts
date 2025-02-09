26dcbbcfb554cf238d1c735cd1facc696190e09f
sima-sah1 <coding962@gmail.com>
2025-02-09 13:49:15 +0545

    git ignore

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,9 +1,10 @@
 import {Router} from 'express'
+import AuthController from '../controllers/userController'
 const router:Router = express.Router()
 
 
 router.route("/register")
-.post(AbortController.registerUser)
+.post(AuthController.registerUser)
 
 
 export default router 
